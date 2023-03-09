87b59590f9fef83bd1e9296fad0277f507655736
Phạm Tuyên <phamthanhtuyen2k8@gmail.com>
2023-03-09 16:20:17 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,40 @@
+var brname = "";
+var d = new Date();
+if(d.getMonth() == 2) {
+    switch(d.getDate()) {
+        case 11:
+            brname = "Kỳ";
+            break;
+        case 12:
+            brname = "Tuấn";
+            break;
+        default:
+            brname = "một thằng nào đó";
+    }
+}
+const open = () => {
+    setTimeout(() => {
+        document.getElementById("myAudio").muted = false;
+        console.log(document.getElementById("myAudio").muted);
+        document.getElementById("myAudio").play();
+    }, 100);
+}
+
+document.onload = open()
+
+document.getElementById("submit").addEventListener("click", () => {
+    var name = document.getElementById("name").value;
+    if(name != brname) alert("Ê cu, tao biết là mày đang lừa tao mà. Mày là " + brname + " mà");
+    else {
+        document.getElementById("myAudio").pause();
+        document.getElementById("namewrap").style.marginTop = "10vh";
+        document.getElementById("namewrap").style.color = "white";
+        document.getElementById("namewrap").style.fontSize = "60px";
+        document.getElementById("namewrap").innerHTML = `Chúc mừng sinh nhật ` + brname + ` nha&#127881;&#x1F389;&#127881;&#x1F389;<br>
+        <video id="myVideo" width="640" height="352" autoplay>
+            <source src="./rr.mp4" type="video/mp4">
+            Your browser does not support HTML5 video.
+        </video>`;
+        document.getElementById("myVideo").play();
+    }
+})
